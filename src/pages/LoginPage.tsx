ada12206fd75a52666d8e50104a3b5c50d07f40f
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-02-10 12:19:33 +0100

    added a form on loginpage with states

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,46 @@
+import { useState } from "react"
 
 
-const Login = () => {
+const LoginPage = () => {
+
+  //states
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError('');
+  }
+
   return (
-    <div>
-    
-      <h1>Loginsidan</h1>
+    <div className="container_login">
+      <div className="login_inputs">
+
+        <h2>Logga in:</h2>
+
+        <form onSubmit={handleSubmit}>
+          {error && (
+            <div className="error-message">
+              {error}
+
+            </div>
+          )}
+
+          <div className="forminput">
+
+            <label htmlFor="email">E-postadress:</label>
+            <input type="email" id="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+
+            <label htmlFor="password">Lösenord:</label>
+            <input type="text" id="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+
+            <button type="submit">Logga in</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
 
-export default Login
+export default LoginPage
