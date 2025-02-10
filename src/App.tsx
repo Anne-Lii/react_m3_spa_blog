ef10b6e789a90070e2512520c0f88ea651bc3cc6
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-02-10 10:53:49 +0100

    added components for Home, Post, Login and Admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  
+import Home from './components/Home';
+import Post from './components/Post';
+import Login from './components/Login';
+import Admin from './components/Admin';
 
+const App: React.FC = () => {
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/post" element={<Post />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
