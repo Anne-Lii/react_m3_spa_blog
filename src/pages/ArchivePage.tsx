70eaee96d935d38d850bd9490b330479cc7a7502
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-02-24 09:36:33 +0100

    when clicking on a post 'läs mer' the post shows on a PostPage

--- src/pages/ArchivePage.tsx
+++ src/pages/ArchivePage.tsx
@@ -1,5 +1,5 @@
 import './ArchivePage.css';
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 type Post = {
--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,11 +1,71 @@
+import './PostPage.css';
+import { useParams, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+type PostType = { 
+  _id: string; 
+  title: string; 
+  description: string; 
+  createdAt: string 
+};
+
+const PostPage = () => {
+
+  const { id } = useParams<{ id: string }>();
+  const [post, setPost] = useState<PostType | null>(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(`https://react-api-m3.onrender.com/post/${id}`);
+        if (!response.ok) {
+          throw new Error("Kunde inte hämta blogginlägget.");
+        }
+        
+        // Försök läsa svaret som text först
+        const text = await response.text();
+        const data = text ? JSON.parse(text) : null;
+      
+        
+        setPost(data);
+      } catch (err) {
+        setError("Kunde inte ladda inlägget.");
+        console.error(err);
+      }
+    };
+
+    fetchPost();
+  }, [id]);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!post) {
+    return <p>Hittar inga inlägg...</p>;
+  }
 
 
-const Post = () => {
   return (
-    <div>
-      <h1>Postsidan</h1>
+    <div className="post-page">
+      <Link to="/">Tillbaka</Link>
+      <h1>{post.title}</h1>
+      <p className="published">
+        Publicerad:{" "}
+        {new Date(post.createdAt).toLocaleString("sv-SE", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        })}
+      </p>
+      <p>{post.description}</p>
+      <Link to="/">Tillbaka</Link>
     </div>
-  )
+  );
 }
 
-export default Post
+export default PostPage;
