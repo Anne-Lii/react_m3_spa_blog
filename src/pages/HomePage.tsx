43cc0362ed913ab55e07b9c3f703f709f2e91f3c
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-02-25 10:18:58 +0100

    added a remove button to every post on homepage that only shows when logged in and the post gets removed when clicking on it

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,10 +5,14 @@ import Archive from '../components/Archive'
 
 const Home = () => {
 
+  //States
   const [posts, setPosts] = useState<{ _id: string; title: string; description: string ; createdAt: string}[]>([]);
   const [error, setError] = useState("");
   const [loading, SetLoading] = useState(true);//loading when init
 
+  const isLoggedIn = !!localStorage.getItem("token")
+  
+  //get posts
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -29,10 +33,30 @@ const Home = () => {
     fetchPosts();
   }, []);
 
-  if (loading) {
-    return <p>Hämtar inlägg...</p>;
-  }
+  //function to delete a post
+  const deletePost = async(postId: string) => {
 
+    const token = localStorage.getItem("token");
+
+    if (!token) 
+      return;
+    try {
+      const response = await fetch(`https://react-api-m3.onrender.com/post/${postId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      if (!response.ok) {
+        throw new Error("Misslyckades att radera inlägget.");
+      }
+      // Uppdatera listan med inlägg efter radering
+      setPosts(prevPosts => prevPosts.filter(post => post._id !== postId));
+    } catch (err) {
+      console.error(err);
+    }
+
+  };
 
   return (
     <div className="home-container">
@@ -49,32 +73,47 @@ const Home = () => {
       </aside>
 
       <main className="content">
-        <h1>Anne-Lii´s Blogg</h1>
-        {error ? (
-          <p>{error}</p>
-        ) : (
-          posts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-
-          .map((post) => (
-            <article key={post._id} className="blog-post">
-              <h2>{post.title}</h2>
-              <p className="published">
-                Publicerad:{" "}
-                {new Date(post.createdAt).toLocaleString("sv-SE", {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit",
-                hour: "2-digit",
-                minute: "2-digit",
-                hour12: false,
-                })}
-              </p>
-              <p>{post.description.length > 200 ? post.description.substring(0, 100) + "..." : post.description}</p>
-              <Link to={`/post/${post._id}`}>Läs mer</Link>
-            </article>
-          ))
-        )}
-      </main>
+      <h1>Anne-Lii´s Blogg</h1>
+      {loading ? (
+        <p className='loading_message'>Hämtar inlägg...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <>
+          
+          {posts
+            .sort(
+              (a, b) =>
+                new Date(b.createdAt).getTime() -
+                new Date(a.createdAt).getTime()
+            )
+            .map((post) => (
+              <article key={post._id} className="blog-post">
+                <h2>{post.title}</h2>
+                <p className="published">
+                  Publicerad:{" "}
+                  {new Date(post.createdAt).toLocaleString("sv-SE", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: false,
+                  })}
+                </p>
+                <p>
+                  {post.description.length > 200
+                    ? post.description.substring(0, 100) + "..."
+                    : post.description}
+                </p>
+                <Link to={`/post/${post._id}`}>Läs mer</Link>
+                <br></br>
+                {isLoggedIn && (<button onClick={()=> deletePost(post._id)}>Radera</button>)}
+              </article>
+            ))}
+        </>
+      )}
+    </main>
 
       <aside className="sidebar right">
       <Archive posts={posts} />
